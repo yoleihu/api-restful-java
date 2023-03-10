8a146472d6b19632ebdf3ef9d65d6a096e27d3bc
Yolanda Ferreira de Souza <75169200+yoleihu@users.noreply.github.com>
2023-03-10 14:03:43 -0300

    feat: create delete and getById methods for address, change responses of address controller methods

--- src/main/java/challenge/java/api/controller/AddressController.java
+++ src/main/java/challenge/java/api/controller/AddressController.java
@@ -1,6 +1,7 @@
 package challenge.java.api.controller;
 
 import challenge.java.api.dto.AddressListDto;
+import challenge.java.api.dto.PersonDto;
 import challenge.java.api.model.Address;
 import challenge.java.api.dto.AddressDto;
 import challenge.java.api.repository.AddressRepository;
@@ -9,8 +10,10 @@ import challenge.java.api.repository.PersonRespository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,34 +29,54 @@ public class AddressController {
 
     @PostMapping
     @Transactional
-    public HttpStatus register(@RequestBody @Valid AddressDto data) throws Exception {
+    public ResponseEntity register(@RequestBody @Valid AddressDto data, UriComponentsBuilder uriBuilder) throws Exception {
         try{
             Person person = personRespository.findById(data.personId()).orElseThrow(RuntimeException::new);
-            if ( data.mainAddress() == true ){
-                Optional<Address> main = repository.findAllByPerson(person)
-                        .stream().filter(a -> a.getMainAddress().equals(true)).findFirst();
 
-                if(main.isPresent()){
-                    main.get().setMainAddress(false);
-                    repository.save(main.get());
-                }
+            Optional<Address> main = repository.findAllByPerson(person)
+                    .stream().filter(a -> a.getMainAddress().equals(true)).findFirst();
+
+            if(main.isPresent() && data.mainAddress()){
+                main.get().setMainAddress(false);
+                repository.save(main.get());
             }
-            repository.save(new Address(data, person));
-            return HttpStatus.CREATED;
+
+            Address address = new Address(data, person);
+            repository.save(address);
+
+            var uri = uriBuilder.path("/address/{id}").buildAndExpand(address.getId()).toUri();
+
+            return ResponseEntity.created(uri).body(new AddressDto(address.getStreet(), address.getZip(), address.getCity(), address.getNumber(), address.getMainAddress(), address.getPerson().getId()));
+
         }
         catch (Exception e) {
             throw new Exception(e);
         }
     }
 
-    @GetMapping("/{id}")
-    public List<AddressListDto> findById(@PathVariable Long id) throws Exception {
+    @GetMapping("/person/{id}")
+    public ResponseEntity<List<AddressListDto>> findByAllByPersonId(@PathVariable Long id) throws Exception {
         try{
-            return repository.findAllByPerson(personRespository.findById(id).get()).
+            List<AddressListDto> addresses = repository.findAllByPerson(personRespository.findById(id).get()).
                     stream().map(AddressListDto::new).toList();
+            return ResponseEntity.ok(addresses);
         }catch (Exception e){
             throw new Exception(e);
         }
 
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Address>> findById(@PathVariable Long id) {
+        Optional<Address> address = repository.findById(id);
+        return ResponseEntity.ok(address);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity delete(@PathVariable Long id) {
+        repository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
