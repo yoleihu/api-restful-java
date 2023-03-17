d5301c207c07f50b09c89ce235044a537551f439
yoleihu <landacontato@gmail.com>
2023-03-17 14:36:09 -0300

    feat: handling to 400 exception

--- src/main/java/challenge/java/api/advice/ControllerAdvice.java
+++ src/main/java/challenge/java/api/advice/ControllerAdvice.java
@@ -2,8 +2,12 @@ package challenge.java.api.advice;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class ControllerAdvice {
@@ -11,5 +15,17 @@ public class ControllerAdvice {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity notFound() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity argumentNotValid(MethodArgumentNotValidException exception) {
+        var errors = exception.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(ArgumentNotValidBody::new).toList());
+    }
+
+    private record ArgumentNotValidBody(String argument, String message) {
+        public ArgumentNotValidBody(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 }
