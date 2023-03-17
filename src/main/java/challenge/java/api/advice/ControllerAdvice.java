d31b1e912fc25fbec49166e608c0236d4d4c2ddf
yoleihu <landacontato@gmail.com>
2023-03-17 11:57:20 -0300

    fix: refactor name ControlerAdvice to ControllerAdvice

--- src/main/java/challenge/java/api/advice/ControllerAdvice.java
+++ src/main/java/challenge/java/api/advice/ControllerAdvice.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ControlerAdvice {
+public class ControllerAdvice {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity notFound() {
